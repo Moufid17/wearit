65d9247f620a84fee67449c77bd4134a8426dfc7
Moufid17 <moufid.mtr@gmail.com>
2021-09-23 20:38:49 +0200

    Set up Register form fields features

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -7,13 +7,51 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('firstname',TextType::class, 
+            [
+                'label' => 'FirstName',
+                'attr' => [
+                    'placeholder' => 'Marc'
+                ]
+            ])
+            ->add('lastname',TextType::class, [
+                'label' => 'LastName',
+                'attr' => [
+                    'placeholder' => 'ADAMS'
+                ]
+            ])
+            ->add('email',EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'marc.adams@xyz.com'
+                ]
+            ])
+            ->add('password',PasswordType::class,[
+                'label' => 'Password',
+                'attr' => [
+                    'placeholder' => '**********'
+                ]
+            ])
+            ->add('cpassword',PasswordType::class,[
+                'label' => 'Confirm Password',
+                'mapped' => false,
+                'attr'  => [
+                    'placeholder' => '**********'
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 'Submit',
+            ])
         ;
     }
 
