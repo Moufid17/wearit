b9abb247e0fab69f1e956226520789d57e5dc96c
Moufid17 <moufid.mtr@gmail.com>
2021-11-07 22:00:46 +0100

    get submitted datas and create doctrine's <entityManager> to persist then flush data to DB.

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,16 +8,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RegisterController extends AbstractController
 {
+
+    /**
+     * @var RegisterRepository
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em){
+        $this->em = $em;
+    }
+
+
     /**
      * @Route("/register", name="register")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class,$user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get data submit
+            $user = $form->getData();
+            
+            // Perssit submitted datas to db.
+            $this->em->persist($user);
+            $this->em->flush();
+            dd($user);
+            
+        }
         return $this->render('register/index.html.twig', 
         [
             'form' => $form->createView(),
